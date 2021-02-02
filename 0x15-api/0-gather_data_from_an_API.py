4329933e02398a3884544fd63377f85371adbf29
cort-robinson <cortrobinson99@gmail.com>
2021-02-02 02:32:32 +0000

    add if __name__ == '__main__':

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,16 +4,17 @@ import requests
 from sys import argv
 
 
-employee_name = ((requests.get(
-    'https://jsonplaceholder.typicode.com/users/{}'.format(
-        argv[1]))).json()).get('name')
-todos = (requests.get(
-    'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
-        argv[1]))).json()
+if __name__ == '__main__':
+    employee_name = ((requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(
+            argv[1]))).json()).get('name')
+    todos = (requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+            argv[1]))).json()
 
-completed = [todo for todo in todos if todo.get('completed') is True]
+    completed = [todo for todo in todos if todo.get('completed') is True]
 
-print("Employee {:s} is done with tasks({}/{}):".format(
-    employee_name, len(completed), len(todos)))
-for task in completed:
-    print("\t {:s}".format(task.get('title')))
+    print("Employee {:s} is done with tasks({}/{}):".format(
+        employee_name, len(completed), len(todos)))
+    for task in completed:
+        print("\t {:s}".format(task.get('title')))
